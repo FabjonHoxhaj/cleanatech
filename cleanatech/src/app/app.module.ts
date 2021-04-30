858e8d039e27092c3de696961e759627d953bae8
U-DESKTOP-SFCGEHP\mikro <fahox1991@gmail.com>
2021-04-30 20:49:00 +0200

    create navigation component

--- cleanatech/src/app/app.module.ts
+++ cleanatech/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
 
   ],
   imports: [
