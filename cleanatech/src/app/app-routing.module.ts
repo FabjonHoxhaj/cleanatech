3b4ce984979fc20defdaa6ac5a36d1be57fd662c
U-DESKTOP-SFCGEHP\mikro <fahox1991@gmail.com>
2021-05-05 20:17:34 +0200

    generate contactComponent and routing configured

--- cleanatech/src/app/app-routing.module.ts
+++ cleanatech/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UnsComponent } from './uns/uns.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [{ path: "", redirectTo: "home", pathMatch: "full" },
 { path: "uns", component: UnsComponent },
-{ path: "home", component: HomeComponent }
+{ path: "home", component: HomeComponent },
+{ path: "kontakt", component: ContactComponent }
 ];
 
 @NgModule({
