72cf7ab5eee73d0e02ac882df1bf2f1ecfc9cbbd
U-DESKTOP-SFCGEHP\mikro <fahox1991@gmail.com>
2021-05-05 19:29:51 +0200

    routing successful

--- cleanatech/src/app/app-routing.module.ts
+++ cleanatech/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UnsComponent } from './uns/uns.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [{ path: " ", redirectTo: "home", pathMatch: "full" }, { path: "uns", component: UnsComponent }];
+const routes: Routes = [{ path: "", redirectTo: "home", pathMatch: "full" },
+{ path: "uns", component: UnsComponent },
+{ path: "home", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
